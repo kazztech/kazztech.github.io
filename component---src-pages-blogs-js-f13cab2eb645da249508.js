eae71ed560f6c5d0b0afcecb81a1e70fc82aedac
kazz <kazzzz1001@gmail.com>
2020-06-15 01:52:02 +0900

    Updates

---
+++ component---src-pages-blogs-js-f13cab2eb645da249508.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{Wo83:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),u=t.n(o);e.default=function(){return u.a.createElement("div",null,"blogs")}}}]);
+//# sourceMappingURL=component---src-pages-blogs-js-f13cab2eb645da249508.js.map
